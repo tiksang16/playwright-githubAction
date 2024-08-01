d8f20f7cf96c950110599e4360258f043df49631
Tik Sang Chan <tiksangchan@tiks-air.home>
2024-08-01 17:53:06 -0400

    added config file

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  projects: [
+    {
+      name: "chromium",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "firefox",
+      use: { browserName: "firefox" },
+    },
+    {
+      name: "webkit",
+      use: { browserName: "webkit" },
+    },
+  ],
+};
+
+export default config;
