f7029aafb64551884e44fb4e2f008fc9fb84136b
Tik Sang Chan <tiksangchan@tiks-air.home>
2024-08-01 17:46:24 -0400

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+test("basic test", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+  const title = await page.title();
+  expect(title).toBe(
+    "Fast and reliable end-to-end testing for modern web apps | Playwright"
+  );
+});
